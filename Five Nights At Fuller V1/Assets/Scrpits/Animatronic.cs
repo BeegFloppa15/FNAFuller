70b2da62dd6637e7f166e79ef752804c73cac9d7
BeegFloppa15 <bjgraham@wpi.edu>
2024-01-10 19:38:36 -0500

    Added Aggression based random movement

--- Five Nights At Fuller V1/Assets/Scrpits/Animatronic.cs
+++ Five Nights At Fuller V1/Assets/Scrpits/Animatronic.cs
@@ -4,21 +4,33 @@ using UnityEngine;
 
 public class Animatronic : MonoBehaviour
 {
-    //Any number from 0-20
+    /**Any number from 0-20 */
     public int agression;
+
     public int locationIndex;
     public GameObject animatronImage;
     [SerializeField] GameObject[] locations;
+    public float moveTimer;
+    public float decisionTime;
 
     // Start is called before the first frame update
     void Start()
     {
         locationIndex = 0;
+        moveTimer = 0.0f;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        moveTimer += Time.deltaTime;
+        if(moveTimer >= decisionTime)
+        {
+            DecideToMove();
+            moveTimer = 0.0f;
+        }
+
         if (locations[locationIndex].activeInHierarchy)
         {
             animatronImage.SetActive(true);
@@ -28,11 +40,22 @@ public class Animatronic : MonoBehaviour
             animatronImage.SetActive(false);
         }
 
+        //This is to be removed once game is finished. It exists now for dev purposes
         if (Input.GetKeyDown("m")) 
         {
             MoveLocation();
         }
 
+    }
+
+    void DecideToMove()
+    {
+        if (Random.Range(1, 20) <= agression)
+        {
+            MoveLocation();
+            Debug.Log("Moved");
+        }
+        Debug.Log("Decision Made");
     }
 
 
